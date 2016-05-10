0715f9c59c2d3e2cb280c1f15899bb44f40c5e65
Anthony Scopatz <scopatz@gmail.com>
2016-05-09 22:43:58 -0400

    try to remove lib64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-./configure --disable-debug --disable-dependency-tracking --prefix=${PREFIX}
+./configure --disable-debug --disable-dependency-tracking --prefix="${PREFIX}" \
+  --exec-prefix="${PREFIX}" --libdir="${PREFIX}/lib"
+  || { cat config.log; exit 1;}
 make
 make check
 make install
 
-if [[ $(uname) == Linux ]]; then
-  mkdir -p ${PREFIX}/lib
-  mv ${PREFIX}/lib64/* ${PREFIX}/lib/
-fi
+#if [[ $(uname) == "Linux" ]]; then
+#  mkdir -p ${PREFIX}/lib
+#  mv ${PREFIX}/lib64/* ${PREFIX}/lib/
+#fi
