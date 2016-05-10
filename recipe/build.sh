465b078545879ec571d9d60e7129fcedf1777f75
Anthony Scopatz <scopatz@gmail.com>
2016-05-09 23:06:52 -0400

    change install loc

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-sed -i 's:@toolexeclibdir@:$(libdir):g' Makefile.in
-#  --exec-prefix="${PREFIX}" --libdir="${PREFIX}/lib" \
+if [[ $(uname) == "Linux" ]]; then
+  # this changes the install dir from ${PREFIX}/lib64 to ${PREFIX}/lib
+  sed -i 's:@toolexeclibdir@:$(libdir):g' Makefile.in
+fi
 ./configure --disable-debug --disable-dependency-tracking --prefix="${PREFIX}" \
   || { cat config.log; exit 1;}
 make
 make check
 make install
-
-#if [[ $(uname) == "Linux" ]]; then
-#  mkdir -p ${PREFIX}/lib
-#  mv ${PREFIX}/lib64/* ${PREFIX}/lib/
-#fi
