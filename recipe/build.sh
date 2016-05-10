80846fdc33a7501530dd1c0918242f64d7b3bf26
Anthony Scopatz <scopatz@gmail.com>
2016-05-10 12:47:04 -0400

    more lib64 fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,8 @@
 
 if [[ $(uname) == "Linux" ]]; then
   # this changes the install dir from ${PREFIX}/lib64 to ${PREFIX}/lib
-  sed -i 's:@toolexeclibdir@:$(libdir):g' Makefile.in
+  sed -i 's:@toolexeclibdir@:$(libdir):g' Makefile.in */Makefile.in
+  sed -i 's:@toolexeclibdir@:${libdir}:g' libffi.pc.in
 fi
 ./configure --disable-debug --disable-dependency-tracking --prefix="${PREFIX}" \
   || { cat config.log; exit 1;}
