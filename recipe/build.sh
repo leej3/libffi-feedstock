79fa9336d6110b767ec4013513a67964aaa0e26b
shadow_walker <shadowwalkersb@gmail.com>
2016-12-28 17:50:10 -0600

    Revert "specify include dir"
    This reverts commit 83dca57b6b82a28ede4b0d6ce51b3a9b679d1594.

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,7 @@ if [[ $(uname) == "Linux" ]]; then
   sed -i 's:@toolexeclibdir@:$(libdir):g' Makefile.in */Makefile.in
   sed -i 's:@toolexeclibdir@:${libdir}:g' libffi.pc.in
 fi
-./configure --disable-debug --disable-dependency-tracking \
-  --prefix="${PREFIX}" --includedir="${PREFIX}/include" \
+./configure --disable-debug --disable-dependency-tracking --prefix="${PREFIX}" \
   || { cat config.log; exit 1;}
 make
 make check
